51268f18e965ae80da72931149f8bd4e55242207
Laily Sarvarian <laily@lailys.com>
2021-08-30 13:32:13 -0700

    Fix some issues on addMethod with animation and add throttle instead of debounce

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const style = document.createElement('style');
 document.getElementsByTagName('head')[0].appendChild(style);
 
 
+let nodeExistMap = {}
+
+function creatAvailableArr() {
+  let i = -100
+  while (i < 101) {
+    nodeExistMap[i] = false
+    i++
+  }
+}
+creatAvailableArr()
 
 class Node {
   constructor(value, level, parent, parentSide, loc) {
@@ -23,68 +33,73 @@ class BinarySearchTree {
     this.root = null;
   }
   baseAddNewNode(value) {
-    if (this.root && this.root.value === value) {
-      return this
-    } else {
-      const newNode = document.createElement("button");
-      newNode.setAttribute("class", ` node node-${value}`);
-      newNode.innerText = value;
-      newNode.onclick = this.deletNode.bind(this, [newNode, value])
-      page.appendChild(newNode)
-      //   availableNums[value + 100]=null;
-      if (this.root === null) {
-        this.root = new Node(value, 1, null, "", [0, w / 2 - 12.5])
-        newNode.style.top = "0px";
-        newNode.style.left = `${w / 2 - 10}px`;
-        return this.root
+    if (!nodeExistMap[value]) {
+      nodeExistMap[value] = true
+      if (this.root && this.root.value === value) {
+        return this
       } else {
-        let currNode = this.root
-        let level = 1
-        while (currNode) {
-          if (currNode.value === value) return
-          if (currNode.value < value) {
-            if (currNode.right === null) {
-              const newLevel = level + 1
-              const numberOfGap = (128 / Math.pow(2, level + 1)) / 2
-              const left = currNode.loc[1] + (numberOfGap * gap)
-              const top = currNode.loc[0] + 40
-              const rightLine = document.createElement("div")
-              rightLine.setAttribute("class", "line right" + ` ${value}`);
-              rightLine.style.top = `${currNode.loc[0]+8}px`;
-              rightLine.style.width = `${numberOfGap * gap}px`;
-              rightLine.style.left = `${currNode.loc[1]+15}px`;
-              rightLine.style.height = "45px"
-              page.appendChild(rightLine)
-              currNode.right = new Node(value, newLevel, currNode, "right", [top, left])
-              newNode.style.top = `${top}px`;
-              newNode.style.left = `${left}px`;
-              return this
-            } else {
-              currNode = currNode.right
-              level++
-            }
-          } else if (currNode.value > value) {
-            if (currNode.left === null) {
-              const newLevel = level + 1
-              const numberOfGap = (128 / Math.pow(2, level + 1)) / 2
-              const left = currNode.loc[1] - (numberOfGap * gap)
-              const top = currNode.loc[0] + 40
+        const newNode = document.createElement("button");
+        newNode.setAttribute("class", ` node node-${value}`);
+        newNode.innerText = value;
+        newNode.onclick = this.deletNode.bind(this, [newNode, value])
 
-              const leftLine = document.createElement("div")
-              leftLine.setAttribute("class", "line left" + ` ${value}`);
-              leftLine.style.top = `${currNode.loc[0]+8}px`;
-              leftLine.style.width = `${(numberOfGap * gap)}px`;
-              leftLine.style.left = `${currNode.loc[1]-((numberOfGap * gap))}px`;
-              leftLine.style.height = "45px"
-              page.appendChild(leftLine)
+        //   availableNums[value + 100]=null;
+        if (this.root === null) {
+          this.root = new Node(value, 1, null, "", [0, w / 2 - 12.5])
+          newNode.style.top = "0px";
+          newNode.style.left = `${w / 2 - 10}px`;
+          page.appendChild(newNode)
+          return this.root
+        } else {
+          let currNode = this.root
+          let level = 1
+          while (currNode) {
+            if (currNode.value === value) return
+            page.appendChild(newNode)
+            if (currNode.value < value) {
+              if (currNode.right === null) {
+                const newLevel = level + 1
+                const numberOfGap = (128 / Math.pow(2, level + 1)) / 2
+                const left = currNode.loc[1] + (numberOfGap * gap)
+                const top = currNode.loc[0] + 40
+                const rightLine = document.createElement("div")
+                rightLine.setAttribute("class", "line right" + ` ${value}`);
+                rightLine.style.top = `${currNode.loc[0]+8}px`;
+                rightLine.style.width = `${numberOfGap * gap}px`;
+                rightLine.style.left = `${currNode.loc[1]+15}px`;
+                rightLine.style.height = "45px"
+                page.appendChild(rightLine)
+                currNode.right = new Node(value, newLevel, currNode, "right", [top, left])
+                newNode.style.top = `${top}px`;
+                newNode.style.left = `${left}px`;
+                return this
+              } else {
+                currNode = currNode.right
+                level++
+              }
+            } else if (currNode.value > value) {
+              if (currNode.left === null) {
+                const newLevel = level + 1
+                const numberOfGap = (128 / Math.pow(2, level + 1)) / 2
+                const left = currNode.loc[1] - (numberOfGap * gap)
+                const top = currNode.loc[0] + 40
 
-              currNode.left = new Node(value, level + 1, currNode, "left", [top, left])
-              newNode.style.top = `${top}px`;
-              newNode.style.left = `${left-5}px`;
-              return this
-            } else {
-              currNode = currNode.left
-              level++
+                const leftLine = document.createElement("div")
+                leftLine.setAttribute("class", "line left" + ` ${value}`);
+                leftLine.style.top = `${currNode.loc[0]+8}px`;
+                leftLine.style.width = `${(numberOfGap * gap)}px`;
+                leftLine.style.left = `${currNode.loc[1]-((numberOfGap * gap))}px`;
+                leftLine.style.height = "45px"
+                page.appendChild(leftLine)
+
+                currNode.left = new Node(value, level + 1, currNode, "left", [top, left])
+                newNode.style.top = `${top}px`;
+                newNode.style.left = `${left-5}px`;
+                return this
+              } else {
+                currNode = currNode.left
+                level++
+              }
             }
           }
         }
@@ -92,136 +107,144 @@ class BinarySearchTree {
     }
   }
   animateAddNewNode(value) {
-    let traversalNodes = [
-      [0, 0]
-    ]
-    if (this.root && this.root.value === value) {
-      return this
-    } else {
-      const newNode = document.createElement("button");
-      newNode.setAttribute("class", ` node node-${value}`);
-      newNode.innerText = value;
-      newNode.onclick = this.deletNode.bind(this, [newNode, value, ])
-      page.appendChild(newNode)
+    if (!nodeExistMap[value]) {
+      nodeExistMap[value] = true
+      let traversalNodes = [
+        [0, 0]
+      ]
+      if (this.root && this.root.value === value) {
+        return this
+      } else {
+        const newNode = document.createElement("button");
+        newNode.setAttribute("class", ` node node-${value}`);
+        newNode.innerText = value;
+        newNode.onclick = this.deletNode.bind(this, [newNode, value, ])
+        page.appendChild(newNode)
+        let currNode = this.root
+        let level = 1
 
-      let currNode = this.root
-      let level = 1
+        while (currNode) {
 
-      while (currNode) {
+          if (currNode.value === value) return
+          if (currNode.value < value) {
+            traversalNodes.push(currNode.loc)
+            if (currNode.right === null) {
+              const newLevel = level + 1
+              const numberOfGap = (128 / Math.pow(2, newLevel)) / 2
+              const left = currNode.loc[1] + (numberOfGap * gap)
+              const top = currNode.loc[0] + 40
+              traversalNodes.push([top, left])
 
-        if (currNode.value === value) return
-        if (currNode.value < value) {
-          traversalNodes.push(currNode.loc)
-          if (currNode.right === null) {
-            const newLevel = level + 1
-            const numberOfGap = (128 / Math.pow(2, newLevel)) / 2
-            const left = currNode.loc[1] + (numberOfGap * gap)
-            const top = currNode.loc[0] + 40
-            traversalNodes.push([top, left])
+              let keyFrames = `
+                @keyframes frame${value} {
+               
+                  `
+              let addsOn = `
+                0% {
+                  background - color: red;
+                  left: 0 px;
+                  top: 0 px;
+                  color:#16A1FF;
+                }
+                `
+              const nodeToTravel = traversalNodes.length
+              const percentage = 100 / (nodeToTravel - 1)
+              let completed = percentage
 
-            let keyFrames = `
-            @keyframes frame${value} {
-           
-              `
-            let addsOn = `
-            0% {
-              background - color: red;
-              left: 0 px;
-              top: 0 px;
-              color:#16A1FF;
+              for (let i = 1; i < nodeToTravel; i++) {
+                i + 1 === nodeToTravel ?
+                  addsOn += ` 
+                    100% {
+                        left:${traversalNodes[i][1]}px;
+                        top:${traversalNodes[i][0]}px;
+                        background-color:${i+1===nodeToTravel?"16a144":"#16A1FF"};
+                    }` :
+                  addsOn += ` 
+                ${completed}% {
+                    left:${traversalNodes[i][1]}px;
+                    top:${traversalNodes[i][0]}px;
+                    background-color:${i+1===nodeToTravel?"16a144":"#16A1FF"};
+                }`
+                completed += percentage
+              }
+              keyFrames += addsOn + "}"
+              style.innerHTML += keyFrames
+              newNode.style.animationName = `frame${value}`;
+              setTimeout(function () {
+                const rightLine = document.createElement("div")
+                rightLine.setAttribute("class", "line right" + ` ${value}`);
+                rightLine.style.top = `${currNode.loc[0]+8}px`;
+                rightLine.style.width = `${numberOfGap * gap}px`;
+                rightLine.style.left = `${currNode.loc[1]+15}px`;
+                rightLine.style.height = "45px"
+                page.appendChild(rightLine)
+              }, 4000)
+
+              currNode.right = new Node(value, newLevel, currNode, "right", [top, left])
+
+              return this
+            } else {
+              currNode = currNode.right
+              level++
             }
-            `
-            const nodeToTravel = traversalNodes.length
-            const percentage = 100 / (nodeToTravel - 1)
-            let completed = percentage
+          } else if (currNode.value > value) {
+            traversalNodes.push(currNode.loc)
+            if (currNode.left === null) {
+              const newLevel = level + 1
+              const numberOfGap = (128 / Math.pow(2, newLevel)) / 2
+              const left = currNode.loc[1] - (numberOfGap * gap)
+              const top = currNode.loc[0] + 40
 
-            for (let i = 1; i < nodeToTravel; i++) {
-              addsOn += ` 
-            ${completed}% {
-                left:${traversalNodes[i][1]}px;
-                top:${traversalNodes[i][0]}px;
-                background-color:${i+1===nodeToTravel?"16a144":"#16A1FF"};
-            }`
-              completed += percentage
+              traversalNodes.push([top, left])
+              let keyFrames = `
+                @keyframes frame${value} {
+                  `
+              let addsOn = `
+                0% {
+                  background - color: red;
+                  left: 0 px;
+                  top: 0 px;
+                  color:#16A1FF;
+                }
+                `
+              const nodeToTravel = traversalNodes.length
+              const percentage = 100 / (nodeToTravel - 1)
+              let completed = percentage
+
+              for (let i = 1; i < nodeToTravel; i++) {
+                i + 1 === nodeToTravel ?
+                  addsOn += ` 
+                100% {
+                    left:${traversalNodes[i][1]}px;
+                    top:${traversalNodes[i][0]}px;
+                    background-color:${i+1===nodeToTravel?"16a144":"#16A1FF"};
+                }` :
+                  addsOn += ` 
+                ${completed}% {
+                    left:${traversalNodes[i][1]}px;
+                    top:${traversalNodes[i][0]}px;
+                    background-color:${i+1===nodeToTravel?"16a144":"#16A1FF"};
+                }`
+                completed += percentage
+              }
+              keyFrames += addsOn + "}"
+              style.innerHTML += keyFrames
+              newNode.style.animationName = `frame${value}`;
+              setTimeout(function () {
+                const leftLine = document.createElement("div")
+                leftLine.setAttribute("class", "line left" + ` ${value}`);
+                leftLine.style.top = `${currNode.loc[0]+8}px`;
+                leftLine.style.width = `${(numberOfGap * gap)}px`;
+                leftLine.style.left = `${currNode.loc[1]-((numberOfGap * gap))}px`;
+                leftLine.style.height = "45px"
+                page.appendChild(leftLine)
+              }, 4000)
+              currNode.left = new Node(value, level + 1, currNode, "left", [top, left])
+              return this
+            } else {
+              currNode = currNode.left
+              level++
             }
-            keyFrames += addsOn + "}"
-            console.log(keyFrames)
-            style.innerHTML += keyFrames
-            newNode.style.animationName = `frame${value}`;
-            setTimeout(function () {
-              const rightLine = document.createElement("div")
-              rightLine.setAttribute("class", "line right" + ` ${value}`);
-              rightLine.style.top = `${currNode.loc[0]+8}px`;
-              rightLine.style.width = `${numberOfGap * gap}px`;
-              rightLine.style.left = `${currNode.loc[1]+15}px`;
-              rightLine.style.height = "45px"
-              page.appendChild(rightLine)
-            }, 4000)
-
-            currNode.right = new Node(value, newLevel, currNode, "right", [top, left])
-
-            return this
-          } else {
-            currNode = currNode.right
-            level++
-          }
-        } else if (currNode.value > value) {
-          traversalNodes.push(currNode.loc)
-          if (currNode.left === null) {
-            const newLevel = level + 1
-            const numberOfGap = (128 / Math.pow(2, newLevel)) / 2
-            const left = currNode.loc[1] - (numberOfGap * gap)
-            const top = currNode.loc[0] + 40
-
-            traversalNodes.push([top, left])
-            let keyFrames = `
-            @keyframes frame${value} {
-              `
-            let addsOn = `
-            0% {
-              background - color: red;
-              left: 0 px;
-              top: 0 px;
-              color:#16A1FF;
-            }
-            `
-            const nodeToTravel = traversalNodes.length
-            const percentage = 100 / (nodeToTravel - 1)
-            let completed = percentage
-
-            for (let i = 1; i < nodeToTravel; i++) {
-              i + 1 === nodeToTravel ?
-                addsOn += ` 
-            100% {
-                left:${traversalNodes[i][1]}px;
-                top:${traversalNodes[i][0]}px;
-                background-color:${i+1===nodeToTravel?"16a144":"#16A1FF"};
-            }` :
-                addsOn += ` 
-            ${completed}% {
-                left:${traversalNodes[i][1]}px;
-                top:${traversalNodes[i][0]}px;
-                background-color:${i+1===nodeToTravel?"16a144":"#16A1FF"};
-            }`
-              completed += percentage
-            }
-            keyFrames += addsOn + "}"
-            style.innerHTML += keyFrames
-            newNode.style.animationName = `frame${value}`;
-            setTimeout(function () {
-              const leftLine = document.createElement("div")
-              leftLine.setAttribute("class", "line left" + ` ${value}`);
-              leftLine.style.top = `${currNode.loc[0]+8}px`;
-              leftLine.style.width = `${(numberOfGap * gap)}px`;
-              leftLine.style.left = `${currNode.loc[1]-((numberOfGap * gap))}px`;
-              leftLine.style.height = "45px"
-              page.appendChild(leftLine)
-            }, 4000)
-            currNode.left = new Node(value, level + 1, currNode, "left", [top, left])
-            return this
-          } else {
-            currNode = currNode.left
-            level++
           }
         }
       }
@@ -244,28 +267,18 @@ tree.baseAddNewNode(42)
 tree.baseAddNewNode(44)
 tree.baseAddNewNode(46)
 
-function debounce(func, wait, immediate) {
-  var timeout;
-
-  return function executedFunction() {
-    var context = this;
-    var args = arguments;
-
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-
-    var callNow = immediate && !timeout;
-
-    clearTimeout(timeout);
-
-    timeout = setTimeout(later, wait);
-
-    if (callNow) func.apply(context, args);
-  };
-};
-var debouncedAddNode = debounce(function (e) {
+function throttled(fn, delay) {
+  let lastCall = 0;
+  return function (...args) {
+    const now = (new Date).getTime();
+    if (now - lastCall < delay) {
+      return;
+    }
+    lastCall = now;
+    return fn(...args);
+  }
+}
+var debouncedAddNode = throttled(function (e) {
   if (e.keyCode === 32) {
     console.log("spacebar was clicked to add a random node")
     const randomNumber = Math.floor(Math.random() * (201)) - 100
@@ -273,5 +286,5 @@ var debouncedAddNode = debounce(function (e) {
     tree.animateAddNewNode(randomNumber)
 
   }
-}, 250);
+}, 2500);
 document.body.addEventListener('keyup', debouncedAddNode)

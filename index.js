12af2f70bab6dbdd99cc5a457ced9212d33ac896
Laily Sarvarian <laily@lailys.com>
2021-08-30 12:08:43 -0700

    Add addMethod with animation on spacebar clicks

--- index.js
+++ index.js
@@ -89,6 +89,69 @@ class BinarySearchTree {
     }
 
   }
+
+  animateAddNewNode(value) {
+    if (this.root && this.root.value === value) {
+      return this
+    } else {
+      const newNode = document.createElement("button");
+      newNode.setAttribute("class", ` node node-${value}`);
+      newNode.innerText = value;
+      newNode.onclick = this.deletNode.bind(this, [newNode, value, ])
+      page.appendChild(newNode)
+      let currNode = this.root
+      let level = 1
+      while (currNode) {
+        if (currNode.value === value) return
+        if (currNode.value < value) {
+          traversalNodes.push(currNode.loc)
+          if (currNode.right === null) {
+            const newLevel = level + 1
+            const numberOfGap = (128 / Math.pow(2, newLevel)) / 2
+            const left = currNode.loc[1] + (numberOfGap * gap)
+            const top = currNode.loc[0] + 40
+
+            let keyFrames = `
+                @keyframes frame${value} {
+                `
+
+            let addsOn = `
+                        0% {
+                          background - color: red;
+                          left: 0 px;
+                          top: 0 px;
+                          color:#16A1FF;
+                        }
+                        `
+
+            const nodeToTravel = traversalNodes.length
+            const percentage = 100 / (nodeToTravel - 1)
+            let completed = percentage
+
+            for (let i = 1; i < nodeToTravel; i++) {
+              addsOn += ` 
+              ${completed}% {
+                  left:${traversalNodes[i][1]}px;
+                  top:${traversalNodes[i][0]}px;
+                  background-color:${i+1===nodeToTravel?"16a144":"#16A1FF"};
+              }`
+              completed += percentage
+            }
+
+            keyFrames += addsOn + "}"
+            style.innerHTML += keyFrames
+            currNode.right = new Node(value, newLevel, currNode, "right", [top, left])
+
+            return this
+          } else {
+            currNode = currNode.right
+            level++
+          }
+        }
+      }
+    }
+
+  }
   deletNode(args, e) {
 
   }
@@ -105,3 +168,27 @@ tree.baseAddNewNode(43)
 tree.baseAddNewNode(42)
 tree.baseAddNewNode(44)
 tree.baseAddNewNode(46)
+
+document.onkeypress(addNodetoTree)
+
+const debounced = scope.debounce(function (e) {
+  if (e.keyCode === 32) {
+    const randomNumber = Math.floor(Math.random() * (201)) - 100
+    console.log(randomNumber)
+  }
+}, 500)
+
+function addNodetoTree(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  debounced(e);
+}
+// document.body.onkeyup = (e) => {
+//   if (e.keyCode === 32) {
+//     console.log("spacebar was clicked to add a random node")
+//     const randomNumber = Math.floor(Math.random() * (201)) - 100
+//     startaddingAnimation(e)
+
+//   }
+// }

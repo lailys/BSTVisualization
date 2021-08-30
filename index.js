3a5bda14067c8396ebd51d8f01156a7c27683673
Laily Sarvarian <laily@lailys.com>
2021-08-30 11:20:20 -0700

    Add addMethod for left child commit

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ class BinarySearchTree {
       newNode.innerText = value;
       newNode.onclick = this.deletNode.bind(this, [newNode, value])
       page.appendChild(newNode)
-      //   availableNums[value + 100]=null;
       if (this.root === null) {
         this.root = new Node(value, 1, null, "", [0, w / 2 - 12.5])
         newNode.style.top = "0px";
@@ -62,14 +61,47 @@ class BinarySearchTree {
               level++
             }
           } else if (currNode.value > value) {
+            if (currNode.left === null) {
+              const newLevel = level + 1
+              const numberOfGap = (128 / Math.pow(2, level + 1)) / 2
+              const left = currNode.loc[1] - (numberOfGap * gap)
+              const top = currNode.loc[0] + 40
 
+              const leftLine = document.createElement("div")
+              leftLine.setAttribute("class", "line left" + ` ${value}`);
+              leftLine.style.top = `${currNode.loc[0]+8}px`;
+              leftLine.style.width = `${(numberOfGap * gap)}px`;
+              leftLine.style.left = `${currNode.loc[1]-((numberOfGap * gap))}px`;
+              leftLine.style.height = "45px"
+              page.appendChild(leftLine)
+
+              currNode.left = new Node(value, level + 1, currNode, "left", [top, left])
+              newNode.style.top = `${top}px`;
+              newNode.style.left = `${left-5}px`;
+              return this
+            } else {
+              currNode = currNode.left
+              level++
+            }
           }
         }
       }
     }
+
+  }
+  deletNode(args, e) {
+
   }
 }
 
 
 const tree = new BinarySearchTree()
 tree.root = tree.baseAddNewNode(60)
+tree.baseAddNewNode(65)
+tree.baseAddNewNode(70)
+tree.baseAddNewNode(50)
+tree.baseAddNewNode(45)
+tree.baseAddNewNode(43)
+tree.baseAddNewNode(42)
+tree.baseAddNewNode(44)
+tree.baseAddNewNode(46)
